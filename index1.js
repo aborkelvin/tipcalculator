befa0d75bad9ea91e91e8795ce7672262e20dd19
Kelvin <aborkelvin@gmail.com>
2022-01-12 09:10:24 +0100

    More design and functionality changes
    Changed the format of data collection from input
    Added window to billy and numy to make them global variables
    Added checks against invalid inputs
    Introduced the calculation for fivepercent though will be modified

--- index1.js
+++ index1.js
@@ -8,7 +8,29 @@ input::-webkit-inner-spin-button {
 //The above code seems not to have worked though,check later
 
 
-function myfunction(form) {
-    var boy = form.majorbill.value;
-    alert('the number is ' + boy );
+function myfunction() {
+    /*var billAmount = form.majorbill.value; 
+    var numberAmount = form.majornumber.value;*/
+    let billAmount = document.getElementById('billl').value;
+    let numberAmount = document.getElementById('numberr').value;
+
+    window.billy = Number(billAmount);
+    window.numy = Number(numberAmount);
+    console.log(billy);
+    if(isNaN(billy)){
+      document.getElementById('check').innerHTML = 'Input a valid amount';
+    }
+    if(numy==0){
+      document.getElementById('check2').innerHTML = "Can't be zero";
+    }
+    
 }
+
+//Creating the function to run the 5% program assuming it has all the variables from myfunction in it
+function fivepercent() {
+    myfunction();
+    var chuks = (5/100)* window.billy;
+    var onoj = chuks/window.numy;
+    document.getElementById("tpp").innerHTML= onoj;
+}
+//fivepercent();
