fbd2b4d955ddadea99f8e3d299ad2cc7aae38a65
Kelvin <aborkelvin@gmail.com>
2022-01-18 19:16:38 +0100

    Pre- responsive functionality finishes
    *Added the error messages to all the percent's functions due to the
    return statement which will help quit the important functions
    *Created a new function for the tip calculation with a parameter,
    the parameter is the percent which is changed for the different percent
    functions
    *The new function is called in the different percent's functions with
    the specific percent as the parameter
    *Added a customize function which changes the display of the custom
    button and the input onclick of the custom button
    *Created a new ishi function which gets the value of the custom input,
    checks for errors, then calculates with the input

--- index1.js
+++ index1.js
@@ -1,12 +1,3 @@
-//Hides the arrows from input number field
-
-/*input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}*/
-//The above code seems not to have worked though,check later
-
 
 function myfunction() {
     /*var billAmount = form.majorbill.value; 
@@ -17,20 +8,118 @@ function myfunction() {
     window.billy = Number(billAmount);
     window.numy = Number(numberAmount);
     console.log(billy);
-    if(isNaN(billy)){
-      document.getElementById('check').innerHTML = 'Input a valid amount';
-    }
-    if(numy==0){
-      document.getElementById('check2').innerHTML = "Can't be zero";
-    }
-    
+}
+
+function calculate(percent) {
+  var chuks = (percent/100)* window.billy;
+  var onoj = chuks/window.numy;
+  var onojas = onoj.toFixed(2);
+  document.getElementById("tpp").innerHTML= "$" + onojas;
+  var chidi = window.billy/window.numy;
+  var ebuka = chidi + onoj;
+  var ebukas = ebuka.toFixed(2);
+  document.getElementById("Ttpp").innerHTML = "$" + ebukas;
 }
 
 //Creating the function to run the 5% program assuming it has all the variables from myfunction in it
 function fivepercent() {
     myfunction();
-    var chuks = (5/100)* window.billy;
-    var onoj = chuks/window.numy;
-    document.getElementById("tpp").innerHTML= onoj;
+    if(isNaN(billy)){
+      document.getElementById('check').innerHTML = 'Input a valid amount';
+      return;
+    }
+    if(numy==0){
+      document.getElementById('check2').innerHTML = "Can't be zero";
+      document.getElementById('numberr').className = "badnumber";
+      return;
+    }
+    /*Since the check above was run on all the percent functions, it would have been best to put it in a function
+      and just call the function in all of them but then the return value that stops the program(function) from
+      continuing would just stop the function containing the check thus it would be left like this till it can 
+      be improved*/
+    calculate(5);
+    /*The code above will be put in a function with a parameter, the parameter will be the percentage which
+      can be changed for the different percentages*/
 }
-//fivepercent();
+
+function tenpercent() {
+    myfunction();
+    if(isNaN(billy)){
+      document.getElementById('check').innerHTML = 'Input a valid amount';
+      return;
+    }
+    if(numy==0){
+      document.getElementById('check2').innerHTML = "Can't be zero";
+      document.getElementById('numberr').className = "badnumber";
+      return;
+    }
+    calculate(10);
+}
+
+function fifteenpercent() {
+  myfunction();
+  if(isNaN(billy)){
+    document.getElementById('check').innerHTML = 'Input a valid amount';
+    return;
+  }
+  if(numy==0){
+    document.getElementById('check2').innerHTML = "Can't be zero";
+    document.getElementById('numberr').className = "badnumber";
+    return;
+  }
+  calculate(15);
+}
+
+function twentyfivep() {
+  myfunction();
+  if(isNaN(billy)){
+    document.getElementById('check').innerHTML = 'Input a valid amount';
+    return;
+  }
+  if(numy==0){
+    document.getElementById('check2').innerHTML = "Can't be zero";
+    document.getElementById('numberr').className = "badnumber";
+    return;
+  }
+  calculate(25);
+}
+
+function fiftyp() {
+  myfunction();
+  if(isNaN(billy)){
+      document.getElementById('check').innerHTML = 'Input a valid amount';
+      return;
+    }
+  if(numy==0){
+    document.getElementById('check2').innerHTML = "Can't be zero";
+    document.getElementById('numberr').className = "badnumber";
+    return;
+  }
+  calculate(50);
+}
+
+
+function customize(){
+  document.getElementById('ugbo').style.display= "none";
+  document.getElementById('fada').style.display = "inherit";
+}
+
+function ishi(){
+  let udoka = document.getElementById('fada').value;
+  console.log(udoka);
+  myfunction();
+  if(isNaN(billy)){
+    document.getElementById('check').innerHTML = 'Input a valid amount';
+    return;
+  }
+  if(numy==0){
+    document.getElementById('check2').innerHTML = "Can't be zero";
+    document.getElementById('numberr').className = "badnumber";
+    return;
+  }
+  if(isNaN(udoka)||udoka == 0) {
+    document.getElementById('teej').innerHTML = "Input a valid percentage";
+    return;
+  }
+  calculate(udoka);
+}
